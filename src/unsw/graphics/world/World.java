45246143bcc7b998bc314262f8c9863001ae0290
carolyn <c.e.day@live.com>
2018-10-01 12:15:34 +1000

    world changes

--- src/unsw/graphics/world/World.java
+++ src/unsw/graphics/world/World.java
@@ -2,15 +2,23 @@ package unsw.graphics.world;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.IntBuffer;
+import java.util.Arrays;
 
 import com.jogamp.newt.event.KeyEvent;
 import com.jogamp.newt.event.KeyListener;
+import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL3;
+import com.jogamp.opengl.util.GLBuffers;
 
 import unsw.graphics.Application3D;
 import unsw.graphics.CoordFrame3D;
 import unsw.graphics.Matrix4;
+import unsw.graphics.Point3DBuffer;
 import unsw.graphics.Shader;
+import unsw.graphics.geometry.Point3D;
+import unsw.graphics.geometry.TriangleMesh;
 import unsw.graphics.world.Camera;
 
 
@@ -24,6 +32,13 @@ public class World extends Application3D implements KeyListener {
 
     private Terrain terrain;
     private Camera camera;
+    private TriangleMesh terrainMesh;
+    private float rotationY;
+    private Point3DBuffer vertexBuffer;
+    private IntBuffer indicesBuffer;
+    private int verticesName;
+    private int indicesName;
+
 
     public World(Terrain terrain) {
     	super("Assignment 2", 800, 600);
@@ -61,10 +76,54 @@ public class World extends Application3D implements KeyListener {
 	@Override
 	public void init(GL3 gl) {
 		super.init(gl);
-        getWindow().addKeyListener(camera);
-        getWindow().addKeyListener(this);
-		
-		
+		getWindow().addKeyListener(camera);
+		getWindow().addKeyListener(this);
+
+		vertexBuffer = new Point3DBuffer(Arrays.asList(
+				new Point3D(-1,-1,1), 
+				new Point3D(1,-1,1), 
+				new Point3D(1,1,1),
+				new Point3D(-1,1,1),
+				new Point3D(-1,-1,-1), 
+				new Point3D(1,-1,-1), 
+				new Point3D(1,1,-1),
+				new Point3D(-1,1,-1)));
+
+		indicesBuffer = GLBuffers.newDirectIntBuffer(new int[] {
+				0,1,2,
+				2,3,0,
+				1,5,6,
+				6,2,1,
+				5,4,7,
+				7,6,5,
+				4,0,3,
+				3,7,4,
+				3,2,6,
+				6,7,3,
+				4,5,1,
+				1,0,4
+		});
+
+		int[] names = new int[2];
+		gl.glGenBuffers(2, names, 0);
+
+		verticesName = names[0];
+		indicesName = names[1];
+
+		gl.glBindBuffer(GL.GL_ARRAY_BUFFER, verticesName);
+		gl.glBufferData(GL.GL_ARRAY_BUFFER, vertexBuffer.capacity() * 3 * Float.BYTES,
+				vertexBuffer.getBuffer(), GL.GL_STATIC_DRAW);
+
+		gl.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, indicesName);
+		gl.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, indicesBuffer.capacity() * Integer.BYTES,
+				indicesBuffer, GL.GL_STATIC_DRAW);
+
+		try {
+			terrainMesh = new TriangleMesh("res/models/cube.ply");
+			terrainMesh.init(gl);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
